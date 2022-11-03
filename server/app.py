2d7a4cb8ffdf76617b2f848e9d260d97e17e2220
S A Sefa <kwabena81@yahoo.com>
2022-11-03 12:59:14 -0400

    ok to unbuntu 22
    On branch transfer-to-debian-11-vm
    Changes to be committed:

--- server/app.py
+++ server/app.py
@@ -8,16 +8,14 @@ def index():
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
 
-
-# @app.errorhandler(404)
-# def not_found(e):
-#     return app.send_static_file('index.html')
-
-
 @app.route('/', methods=['GET'])
 def index():
     return ('Hello')
 
+
+# @app.errorhandler(404)
+# def not_found(e):
+#     return app.send_static_file('index.html')
 
 # @app.route('/api/time')
 # def get_current_time():
